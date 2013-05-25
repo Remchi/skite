950dc0dc3f7eb4409ed737c2e211acea9c9c938a
Rem Zolotykh <remchi@gmail.com>
2013-05-25 23:52:40 +1100

    Foundation framework install

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :assets do
   # gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
+  gem 'zurb-foundation'
 end
 
 gem 'jquery-rails'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require handlebars
 //= require ember
 //= require ember-data
@@ -19,3 +20,5 @@
 //= require skite
 
 Skite = Ember.Application.create();
+
+$(function(){ $(document).foundation(); });
