0f0ace41eba05fbbb0419e88a30f99cd32aecfb5
Rem Zolotykh <remchi@gmail.com>
2013-05-25 23:02:36 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skite::Application.config.secret_token = 'ea9b4b9b172ae01d1b7199637491ced2bdfc979d216e89b6da5d97786150d99debc06b02f9453a3ec8b732ac34f5d07fdf22f3f14524c0ed2ab45ab3ccbd1070'
