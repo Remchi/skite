010952804a5b46227df0fb3913c26f1a93000eeb
Rem Zolotykh <remchi@gmail.com>
2013-05-27 13:20:30 +1100

    Rails achievement create action

--- app/controllers/api/achievements_controller.rb
+++ app/controllers/api/achievements_controller.rb
@@ -5,5 +5,15 @@ module Api
     def index
       respond_with Achievement.find(:all)
     end
+
+    def create
+      respond_with :api, Achievement.create(achievement_params)
+    end
+
+    private
+
+    def achievement_params
+      params.require(:achievement).permit(:title, :achieved_at)
+    end
   end
 end
