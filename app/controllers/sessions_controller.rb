d0dae9018558b65ee49b906ebd2b4f5ef7a16e5d
Rem Zolotykh <remchi@gmail.com>
2013-05-29 02:11:18 +1100

    Sign in happy path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < Devise::SessionsController
   def create
-    render json: { auth_token: 'hello' }, status: 201
+    res = User.find_for_database_authentication(email: params[:email])
+
+    if res && res.valid_password?(params[:password])
+      user = res
+    end
+
+    user.ensure_authentication_token!
+    render json: { auth_token: user.authentication_token }, status: 201
   end
 end
